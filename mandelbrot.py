fb791b6cf582bf9ef7b588796f529cc262580fdd
tsburke86 <72783024+tsburke86@users.noreply.github.com>
2020-11-21 20:38:00 -0800

    Add files via upload

---
+++ mandelbrot.py
@@ -0,0 +1,15 @@
+from math import log, log2
+
+MAX_ITER = 120
+
+def mandelbrot(c):
+    z = 0
+    n = 0
+    while abs(z) <= 2 and n < MAX_ITER:
+        z = z*z + c
+        n += 1
+
+    if n == MAX_ITER:
+        return MAX_ITER
+    
+    return n + 1 - log(log2(abs(z)))
